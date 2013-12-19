77dc654589155dee2e4bc3f3fe6f111b85efc3f6
Victoria Bagnell <victoria@jbyte.com>
2013-12-19 04:23:40 -0500

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+	# Verifies something is written in the columns listed below before entered into the database
+	validates :title, :description, :image_url, presence: true
+
+	# Verifies the price column is a positive number
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+	# Validates unique title
+	validates :title, uniqueness: true
+
+	# Validates an image file or blank
+	validates :image_url, allow_blank: true, format: {
+		# Uses regular expression
+		with: %r{\.(gif|jpg|png)\Z}i,
+		#  Outputs a message
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
+
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    # Sets update equal to some correct value for testing
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Something else!',
+      image_url: 'lorem.jpg',
+      price: 19.84
+    }
   end
 
   test "should get index" do
@@ -18,7 +25,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      #  Calls update and tests it when it creates a product
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +43,8 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    #  Set product to sample update
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
